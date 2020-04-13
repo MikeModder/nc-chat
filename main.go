693ebc676c5ef870f4e5643413c7b702b5d45216
MikeModder <oddman56@protonmail.com>
2020-04-12 21:47:43 -0400

    allow setting listen addr and port

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/MikeModder/nc-chat/server"
 )
 
 func main() {
+	addr := flag.String("addr", "0.0.0.0", "Listen address for the server")
+	port := flag.Int("port", 5006, "Port to run the server on")
+	flag.Parse()
+
 	app := server.NewServer()
-	app.Run("0.0.0.0", 5006)
+	app.Run(*addr, *port)
 }
